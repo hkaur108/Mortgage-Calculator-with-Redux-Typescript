736b81aa687fe7f528dd2e68816bf4a1e4d14914
hkaur108 <hkaur108@hotmail.com>
2025-02-21 16:46:33 -0500

    adding empty result image

--- src/components/ShowResults.tsx
+++ src/components/ShowResults.tsx
@@ -2,7 +2,7 @@ export default function ShowResults() {
   return (
           <aside id='aside' className="px-2 py-3 my-5 text-center" aria-label="Empty result container">
             <figure>
-              <img className="empty-image" src='/assets/images/illustration-empty.svg' alt='empty result image' style={{width:"125px"}}/>
+              <img className="empty-image" src={process.env.PUBLIC_URL+'/assets/images/illustration-empty.svg'} alt='empty result image' style={{width:"125px"}}/>
               <figcaption className='d-none'>this is the empty result image</figcaption>
             </figure>
               <h2 className='fw-bold fs-5'>Results shown here</h2>
